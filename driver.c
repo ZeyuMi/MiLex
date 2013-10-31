0e7cb470658b68315fa6eee5a817537816ae2c27
yzmizeyu <yzmizeyu@gmail.com>
2013-11-01 00:22:39 +0800

    read from file and output to file

--- driver.c
+++ driver.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 #include"token.h"
 
-int main(){
-	int tokenname;
-	FILE *file = fopen("driver.c", "r");
-	setIn(file);
-	while((tokenname=getToken()) != EOF)
-		printf("%d : %s\n", tokenname, token);
-	unsetIn();
-	fclose(file);
+int main(int argc, char *argv[]){
+	if(argc == 1){
+		fprintf(stderr, "ERROR: not enough parameters\n");
+	}else{
+		FILE *outputfile = stdout;
+		while(--argc > 0){
+			char *param = *++argv;
+			if(strcmp(param, "-o") == 0){
+				argc--;
+				outputfile = fopen(*++argv, "w");
+			}else{
+				char *filename = param;
+				fprintf(outputfile, "file %s\n", filename);
+				int tokenname;
+				FILE *file = fopen(filename, "r");
+				setIn(file);
+				while((tokenname=getToken()) != EOF)
+					fprintf(outputfile,"%d : %s\n", tokenname, token);
+				unsetIn();
+				fclose(file);
+			}
+		}
+		fclose(outputfile);
+	}
 	return 0;
 }
