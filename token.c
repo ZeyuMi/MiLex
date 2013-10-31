8b345f2c4cd089d87f106e282021f47d43985eb1
yzmizeyu <yzmizeyu@gmail.com>
2013-10-31 21:46:00 +0800

    now can recognize number and some operator

--- token.c
+++ token.c
@@ -15,6 +15,14 @@ enum states {
 			STATE7,
 			STATE8,
 			STATE9,
+			STATE10,
+			STATE11,
+			STATE12,
+			STATE13,
+			STATE14,
+			STATE15,
+			STATE16,
+			STATE17,
 			};
 
 int state = STATE0;
@@ -40,6 +48,30 @@ int getToken(){
 				}else if(c == '_' || isalpha(c)){
 					state = STATE9;
 					*p++ = c;
+				}else if(isdigit(c)){
+					state = STATE10;
+					*p++ = c;
+				}else if(c == '.'){
+					state = STATE11;
+					*p++ = c;
+				}else if(c == '='){
+					state = STATE12;
+					*p++ = c;
+				}else if(c == '>'){
+					state = STATE13;
+					*p++ = c;
+				}else if(c == '<'){
+					state = STATE14;
+					*p++ = c;
+				}else if(c == '+'){
+					state = STATE15;
+					*p++ = c;
+				}else if(c == '-'){
+					state = STATE16;
+					*p++ = c;
+				}else if(c == '*' || c == '%'){
+					state = STATE17;
+					*p++ = c;
 				}else{
 					state = STATE0;
 				}
@@ -48,12 +80,19 @@ int getToken(){
 				if(c == '*'){
 					state = STATE2;
 				}else if(c == '/'){
-					state = STATE3;
+					state = STATE3;	
+				}else if(c == '='){
+					state = STATE17;
+					*p++ = '/';
+					*p++ = '=';
+					*p = '\0';
+					return NUMOP;
 				}else{
+					state = STATE0;
 					*p++ = '/';
 					*p = '\0';
 					ungetch(c);
-					return SLASH;
+					return NUMOP;
 				}
 				break;
 			case STATE2:
@@ -106,6 +145,129 @@ int getToken(){
 					*p = '\0';
 					ungetch(c);
 					return ID;
+				}
+				break;
+			case STATE10:
+				if(isdigit(c)){
+					state = STATE10;
+					*p++ = c;
+				}else if(c == '.'){
+					state = STATE11;
+					*p++ = c;
+				}else{
+					state = STATE0;
+					*p = '\0';
+					ungetch(c);
+					return NUMBER;
+				}
+				break;
+			case STATE11:
+				if(isdigit(c)){
+					state = STATE11;
+					*p++ = c;
+				}else{
+					state = STATE0;
+					*p = '\0';
+					ungetch(c);
+					return NUMBER;
+				}
+				break;
+			case STATE12:
+				if(c == '='){
+					state = STATE0;
+					*p++ = c;
+					*p = '\0';
+					return LOGICOP;
+				}else{
+					state = STATE0;
+					*p = '\0';
+					ungetch(c);
+					return ASSIGNMENT;
+				}
+				break;
+			case STATE13:
+				if(c == '>'){
+					state = STATE0;
+					*p++ = c;
+					*p = '\0';
+					return BITOP;
+				}else if(c == '='){
+					state = STATE0;
+					*p++ = c;
+					*p = '\0';
+					return LOGICOP;
+				}else{
+					state = STATE0;
+					*p = '\0';
+					ungetch(c);
+					return LOGICOP;
+				}
+				break;
+			case STATE14:
+				if(c == '<'){
+					state = STATE0;
+					*p++ = c;
+					*p = '\0';
+					return BITOP;
+				}else if(c == '='){
+					state = STATE0;
+					*p++ = c;
+					*p = '\0';
+					return LOGICOP;
+				}else{
+					state = STATE0;
+					*p = '\0';
+					ungetch(c);
+					return LOGICOP;
+				}
+				break;
+			case STATE15:
+				if(c == '='){
+					state = STATE0;
+					*p++ = c;
+					*p = '\0';
+					return NUMOP;
+				}else if(c == '+'){
+					state = STATE0;
+					*p++ = c;
+					*p = '\0';
+					return NUMOP;
+				}else{
+					state = STATE0;
+					*p = '\0';
+					ungetch(c);
+					return NUMOP;
+				}
+				break;
+			case STATE16:
+				if(c == '='){
+					state = STATE0;
+					*p++ = c;
+					*p = '\0';
+					return NUMOP;
+				}else if(c == '-'){
+					state = STATE0;
+					*p++ = c;
+					*p = '\0';
+					return NUMOP;
+				}else{
+					state = STATE0;
+					*p = '\0';
+					ungetch(c);
+					return NUMOP;
+				}
+				break;
+			case STATE17:
+				if(c == '='){
+					state = STATE0;
+					*p++ = c;
+					*p = '\0';
+					return NUMOP;
+				}else{
+					state = STATE0;
+					*p = '\0';
+					ungetch(c);
+					return NUMOP;
 				}
 				break;
 			default:
--- token.h
+++ token.h
@@ -1,6 +1,9 @@
 #define ID 1
-#define SLASH 2
-#define NOID 3
+#define NUMBER 2
+#define LOGICOP 3
+#define ASSIGNMENT 4
+#define BITOP 5
+#define NUMOP 6
 #define TOKENSIZE 100
 
 int getToken();
