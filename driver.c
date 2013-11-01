223efa7f3f2abb8899832507e47be722640b5318
yzmizeyu <yzmizeyu@gmail.com>
2013-11-01 10:38:13 +0800

    command line input to tell program about mutiple input files and one output

--- driver.c
+++ driver.c
@@ -1,29 +1,47 @@
 #include <stdio.h>
 #include <string.h>
 #include"token.h"
+#include"mem.h"
 
 int main(int argc, char *argv[]){
+	char *outputfilename = NULL;
+	char *inputfilenames[10];
+	int inputnum = 0;
 	if(argc == 1){
 		fprintf(stderr, "ERROR: not enough parameters\n");
 	}else{
-		FILE *outputfile = stdout;
 		while(--argc > 0){
 			char *param = *++argv;
 			if(strcmp(param, "-o") == 0){
 				argc--;
-				outputfile = fopen(*++argv, "w");
+				outputfilename = alloc(strlen(*++argv)+1);
+				strcpy(outputfilename, *argv);
 			}else{
-				char *filename = param;
-				fprintf(outputfile, "file %s\n", filename);
-				int tokenname;
+				*(inputfilenames+inputnum) = alloc(strlen(param)+1);
+				strcpy(*(inputfilenames+inputnum), param);
+				inputnum++;
+			}
+		}
+		FILE *outputfile = NULL;
+		if(outputfilename == NULL)
+			outputfile = stdout;
+		else
+			outputfile = fopen(outputfilename, "w");
+		int i = 0;
+		while(i < inputnum){
+				char *filename = *(inputfilenames + i);
 				FILE *file = fopen(filename, "r");
+				int tokenname;
+				fprintf(outputfile, "file %s\n", filename);
 				setIn(file);
 				while((tokenname=getToken()) != EOF)
 					fprintf(outputfile,"%d : %s\n", tokenname, token);
 				unsetIn();
 				fclose(file);
-			}
+				afree(filename);
+				i++;
 		}
+		afree(outputfilename);
 		fclose(outputfile);
 	}
 	return 0;
--- makefile
+++ makefile
@@ -1,12 +1,14 @@
-OBJS = driver.o token.o
+OBJS = driver.o token.o mem.o
 
 lex : $(OBJS)
 	cc -g $(OBJS) -o lex
 
-driver.o : token.h token.c driver.c
-	cc -g -c token.c driver.c
+driver.o : token.h token.c mem.h mem.c driver.c
+	cc -g -c token.c mem.c driver.c
 token.o : token.h token.c
 	cc -g -c  token.c
+mem.o : mem.h mem.c
+	cc -g -c mem.c
 
 .PHONY : clean
 clean:
